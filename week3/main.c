2cff1a7fda803205ea5138246d3571efeb6ef7f4
jsy <49744542+choseoyeong@users.noreply.github.com>
2020-09-22 20:13:36 +0900

    Update main.c

--- week3/main.c
+++ week3/main.c
@@ -14,9 +14,10 @@ int main(void)
   RCC_APB2_ENR = 0x00000030; // Port C,D Clock enable 
  
   GPIOC_CRL = 0x00888800; // Joystick(Port D 2,3,4,5) Pull-up/Pull-down Input mode set 
+  // GPIOC_CRL = GPIOC_CRL ^ 0x44CCCC44 (bit 연산 사용)
   
   GPIOD_CRL = 0x10011100; // LED(Port C 2,3,4,7) Output mode set
-  
+  // GPIOD_CRL = GPIOD_CRL ^ 0x54455544  (bit 연산 사용)
    
   while(1) {
     
